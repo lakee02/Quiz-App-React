5a5c0ea618362a2aacf121af5006d7e8caa8626c
lakee mangal <lakee.12002794@lpu.in>
2023-02-05 14:26:11 +0530

    add questions

--- src/App.js
+++ src/App.js
@@ -5,10 +5,65 @@ import Results from './Results';
 
 const questions = [
   {
-    question: 'What is your favorite color?',
-    options: ['Red', 'Green', 'Blue', 'Yellow'],
-    answer: 'Blue',
+    question: 'How do you center an element horizontally in CSS?',
+    options: ['text-align: center;', 'margin: 0 auto;', 'display: flex; justify-content: center;', 'float: center;'],
+    answer: 'margin: 0 auto;',
   },
+  {
+    question: 'What is the purpose of using a CSS reset?',
+    options: [
+      'To remove all default styling from the browser',
+      'To add default styling to the browser',
+      'To change the font of the text',
+      'To change the color of the text',
+    ],
+    answer: 'To remove all default styling from the browser',
+  },
+  {
+    question: 'What is the correct syntax for defining a CSS class?',
+    options: ['.class { }', '#class { }', 'class { }', '*class { }'],
+    answer: '.class { }',
+  },
+  {
+    question: 'What is the default display value of a div element in HTML?',
+    options: ['inline', 'block', 'inline-block', 'none'],
+    answer: 'block',
+  },
+  {
+    question: 'How do you set a background color for a whole page in CSS?',
+    options: [
+      'body { background-color: #ffffff; }',
+      'html { background-color: #ffffff; }',
+      'div { background-color: #ffffff; }',
+      'page { background-color: #ffffff; }',
+    ],
+    answer: 'body { background-color: #ffffff; }',
+  },
+  {
+    question: 'What is the correct syntax for adding a comment in CSS?',
+    options: ['// comment', '/* comment */', '<!-- comment -->', '// comment //'],
+    answer: '/* comment */',
+  },
+  {
+    question: 'What is the CSS property used to change the text color?',
+    options: ['color', 'text-color', 'font-color', 'text-colour'],
+    answer: 'color',
+  },
+  {
+    question: 'What is the correct HTML for creating a hyperlink?',
+    options: ['<a>link</a>', '<link>', '<a href="">link</a>', '<a name="">link</a>'],
+    answer: '<a href="">link</a>',
+  },
+  {
+    question: 'What does CSS stand for?',
+    options: ['Cascading Style Sheets', 'Cascading Style Standards', 'Cascading Styles Syntax', 'Cascading Style Syntax'],
+    answer: 'Cascading Style Sheets',
+  },
+  {
+    question: 'What is the default value of the position property in CSS?',
+    options: ['relative', 'absolute', 'fixed', 'static'],
+    answer: 'static',
+  }
   // Add more questions here
 ];
 
